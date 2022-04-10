d41589b3de4642ce3415457aa5705bec000be545
Baejb <bjb6478@naver.com>
2022-04-10 16:19:52 +0900

    Category 모델 생성

--- blog/models.py
+++ blog/models.py
@@ -5,7 +5,12 @@ from django.db import models
 from turtle import mode
 
 # Create your models here.
+class Category(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
 
+    def __str__(self):
+        return self.name
 
 
 class Post(models.Model):
