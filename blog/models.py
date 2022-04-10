6752cbc6b38c5859cf12aa8c8af1ae94b4501de5
Baejb <bjb6478@naver.com>
2022-04-10 16:13:05 +0900

    post에 author필드 추가

--- blog/models.py
+++ blog/models.py
@@ -1,10 +1,12 @@
 import os.path
 
+from django.contrib.auth.models import User
 from django.db import models
 from turtle import mode
 
 # Create your models here.
-
+class Category(models.Model):
+    
 
 class Post(models.Model):
     title = models.CharField(max_length=30)
@@ -12,6 +14,7 @@ class Post(models.Model):
 
     hook_msg = models.TextField(blank=True)
 
+    author = models.ForeignKey(User, null=True, on_delete=models.CASCADE)
     head_image = models.ImageField(upload_to='blog/images/%Y/%m/%d/', blank=True)
     attached_file = models.FileField(upload_to='blog/files/%Y/%m/%d', blank=True)
     created_at = models.DateTimeField(auto_now_add=True)
@@ -20,7 +23,7 @@ class Post(models.Model):
     # author : 추후 작성 예정
 
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title} :: {self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}/'
