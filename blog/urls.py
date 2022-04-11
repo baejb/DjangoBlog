699b02e8526270763c313bdd412ebd651a8ab3c3
Baejb <bjb6478@naver.com>
2022-04-11 18:41:42 +0900

    category view 생성, tag 모델 추가

--- blog/urls.py
+++ blog/urls.py
@@ -7,4 +7,5 @@ urlpatterns = [
     path('', views.PostList.as_view()),
     #path('<int:pk>/', views.single_post_page),
     #path('', views.index),
+    path( 'category/<str:slug>/',views.show_category_posts)
 ]
