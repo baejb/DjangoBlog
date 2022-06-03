7ae0fc0df1b376a28e70cb037a88700a4510be21
Baejb <bjb6478@naver.com>
2022-06-03 16:54:36 +0900

    로그인기능 생성

--- blog_main/settings.py
+++ blog_main/settings.py
@@ -10,6 +10,7 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 import os.path
+import dj_database_url
 from pathlib import Path
 import os
 
@@ -42,6 +43,11 @@ INSTALLED_APPS = [
     'single_pages',
     'crispy_forms',
     'markdownx',
+    'django.contrib.sites',
+    'allauth',
+    'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.google',
 ]
 
 MIDDLEWARE = [
@@ -134,5 +140,17 @@ STATICFILES_STORAGE = 'whitenoise.storage.CompressedStaticFilesStorage'
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 MEDIA_URL ="/media/"
-MEDIA_ROOT =os.path.join(BASE_DIR, '_media')
+#MEDIA_ROOT =os.path.join(BASE_DIR, '_media')
+MEDIA_ROOT = '/var/vcap/media'
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
+
+AUTHENTICATION_BACKENDS=(
+    'django.contrib.auth.backends.ModelBackend',
+    'allauth.account.auth_backends.AuthenticationBackend'
+)
+
+SITE_ID = 1
+
+ACCOUNT_EMAIL_REQUIRED =True
+ACCOUNT_EMAIL_VERIFICATION ='none'
+LOGIN_REDIRECT_URL='/blog/'
